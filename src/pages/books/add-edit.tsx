9162743916cbeb887d252ed28cd50547d3e9220d
shaneevanz <shaneevanz1997@gmail.com>
2024-05-18 22:23:42 +0530

    added: new book functionality

--- src/pages/books/add-edit.tsx
+++ src/pages/books/add-edit.tsx
@@ -1,14 +1,22 @@
 import { Book } from "@/models/Book";
-import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { CgClose } from "react-icons/cg";
+import db from "../../config/firebase.config";
 
 type Props = {
   bookData?: Book;
+  setModal: Dispatch<SetStateAction<boolean>>;
 };
 
-const AddEdit: React.FC<Props> = ({ bookData }) => {
-  const [book, setBook] = useState<Book | null>(null);
+const AddEdit: React.FC<Props> = ({ bookData, setModal }) => {
+  const [book, setBook] = useState<Book>({
+    author: "",
+    title: "",
+  });
 
-  const onSave = () => {
+  const onSave = (e: any) => {
+    e.preventDefault();
     if (bookData) {
       updateBook();
     } else {
@@ -16,20 +24,43 @@ const AddEdit: React.FC<Props> = ({ bookData }) => {
     }
   };
 
-  const createBook = () => {};
+  const createBook = async () => {
+    await addDoc(collection(db, "books"), book);
+  };
 
   const updateBook = () => {};
 
   return (
-    <div>
-      <form
-        onSubmit={() => {
-          onSave;
-        }}
+    <div className="bg-white/50 fixed top-0 left-0 w-screen h-screen flex justify-center items-center">
+      <button
+        className="absolute top-10 right-10 text-2xl text-black"
+        onClick={() => setModal(false)}
       >
-        <input name="title" type="text" />
-        <input name="author" type="text" />
-        <button type="submit" className="px-4 py-3 rounded bg-green-500">Save</button>
+        <CgClose />
+      </button>
+      <form
+        onSubmit={(e) => {
+          onSave(e);
+        }}
+        className="bg-black p-10 rounded flex flex-col gap-2"
+      >
+        <label htmlFor="title">Title</label>
+        <input
+          className="p-2 bg-black border rounded border-white"
+          name="title"
+          type="text"
+          onChange={(e) => setBook({ ...book, title: e.target.value })}
+        />
+        <label htmlFor="title">Author</label>
+        <input
+          className="p-2 bg-black border rounded border-white"
+          name="author"
+          type="text"
+          onChange={(e) => setBook({ ...book, author: e.target.value })}
+        />
+        <button type="submit" className="px-4 py-3 rounded bg-green-500 my-2">
+          Save
+        </button>
       </form>
     </div>
   );
--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -3,9 +3,11 @@ import "../../app/globals.css";
 import { Book } from "@/models/Book";
 import { collection, getDocs, query } from "firebase/firestore";
 import db from "../../config/firebase.config";
+import AddEdit from "./add-edit";
 
 const books = () => {
   const [books, setBooks] = useState<Book[]>([]);
+  const [addEditModal, setAddEditModal] = useState<boolean>(false);
 
   useEffect(() => {
     const getBooks = async () => {
@@ -24,7 +26,12 @@ const books = () => {
   return (
     <main className="flex justify-center items-center h-screen">
       <div className="p-10 border boder-white rounded w-1/3">
-        <h3 className="font-semibold text-lg mb-3">Book List</h3>
+        <div className="flex justify-between">
+          <h3 className="font-semibold text-lg mb-3">Book List</h3>
+          <button className="bg-green-600 font-semibold px-3 py-2 rounded" onClick={() => setAddEditModal(true)}>
+            New Book
+          </button>
+        </div>
         <table className="w-full text-left">
           <thead>
             <tr className="border-b-2">
@@ -42,6 +49,11 @@ const books = () => {
           </tbody>
         </table>
       </div>
+      {
+        addEditModal && (
+          <AddEdit setModal={setAddEditModal} />
+        )
+      }
     </main>
   );
 };
