14cc7ba1e5b11474fa3d4ec4d8c33bf93dc7667b
shaneevanz <shaneevanz1997@gmail.com>
2024-05-22 01:34:42 +0530

    added: pagination and filtration

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -4,40 +4,66 @@ import { Book } from "@/models/Book";
 import { collection, getDocs, query } from "firebase/firestore";
 import db from "../../config/firebase.config";
 import AddEdit from "./add-edit";
-import { FaRegEdit, FaTrash } from "react-icons/fa";
+import { FaRegEdit, FaSort, FaTrash } from "react-icons/fa";
 
 const books = () => {
   const [books, setBooks] = useState<Book[]>([]);
   const [addEditModal, setAddEditModal] = useState<boolean>(false);
   const [selectedBook, setSelectedBook] = useState<Book | null>(null);
+  const [page, setPage] = useState<number>(1);
+  const [sort, setSort] = useState<
+    "titleAsc" | "authorAsc" | "titleDesc" | "authorDesc"
+  >("titleAsc");
 
   useEffect(() => {
     const getBooks = async () => {
       const snapshot = await getDocs(query(collection(db, "books")));
-      const booksRes = snapshot.docs.map((doc) => ({
+      let booksRes: Book[];
+      booksRes = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       })) as Book[];
-      setBooks(booksRes);
+
+      booksRes.sort((a, b) => {
+        const titleA = a.title.toUpperCase();
+        const titleB = b.title.toUpperCase();
+        const authorA = a.author.toUpperCase();
+        const authorB = b.author.toUpperCase();
+
+        switch (sort) {
+          case "titleAsc":
+            return titleA < titleB ? -1 : titleA > titleB ? 1 : 0;
+          case "titleDesc":
+            return titleA > titleB ? -1 : titleA < titleB ? 1 : 0;
+          case "authorAsc":
+            return authorA < authorB ? -1 : authorA > authorB ? 1 : 0;
+          case "authorDesc":
+            return authorA > authorB ? -1 : authorA < authorB ? 1 : 0;
+          default:
+            return 0;
+        }
+      });
+
+      setBooks(booksRes.slice((page - 1) * 8, page * 8));
     };
 
     getBooks();
-  }, [addEditModal]);
+  }, [addEditModal, page, sort]);
 
   useEffect(() => {
-    if(!addEditModal) {
+    if (!addEditModal) {
       setSelectedBook(null);
     }
-  }, [setAddEditModal])
+  }, [setAddEditModal]);
 
   const openEditModal = (book: Book) => {
     setSelectedBook(book);
     setAddEditModal(true);
-  }
+  };
 
   return (
     <main className="flex justify-center items-center h-screen">
-      <div className="p-10 border boder-white rounded w-1/3 h-3/4">
+      <div className="p-10 border boder-white rounded w-1/3 h-1/2 relative">
         <div className="flex justify-between">
           <h3 className="font-semibold text-lg mb-3">Book List</h3>
           <button
@@ -50,9 +76,29 @@ const books = () => {
         <table className="w-full text-left">
           <thead>
             <tr className="border-b-2">
-              <th className="w-1/2 py-5 px-2">Title</th>
-              <th className="w-1/2 py-5 px-2">Author</th>
-              <th className="w-1/2 py-5 px-2"></th>
+              <th className="py-5 px-2 w-2/5">
+                <span>Title</span>
+                <button
+                  onClick={() =>
+                    setSort(sort === "titleAsc" ? "titleDesc" : "titleAsc")
+                  }
+                  className="float-right pt-1"
+                >
+                  <FaSort />
+                </button>
+              </th>
+              <th className="py-5 px-2 w-2/5">
+                <span>Author</span>
+                <button
+                  onClick={() =>
+                    setSort(sort === "authorAsc" ? "authorDesc" : "authorAsc")
+                  }
+                  className="float-right pt-1"
+                >
+                  <FaSort />
+                </button>
+              </th>
+              <th className="w-1/2 py-5 px-2" colSpan={0}></th>
             </tr>
           </thead>
           <tbody>
@@ -72,13 +118,30 @@ const books = () => {
             ))}
           </tbody>
         </table>
-        <div className="flex justify-end gap-2 w-full py-6">
-          <button className="py-1 px-2 rounded text-center border">1</button>
-          <button className="py-1 px-2 rounded text-center border">2</button>
-          <button className="py-1 px-2 rounded text-center border">3</button>
+        <div className="flex justify-end gap-2 w-full py-6 absolute bottom-2 right-6">
+          {page > 1 && (
+            <button
+              onClick={() => setPage((page) => page - 1)}
+              className="py-1 px-2 rounded text-center border hover:bg-gray-700"
+            >
+              {page - 1}
+            </button>
+          )}
+          <button className="py-1 px-2 rounded text-center border hover:bg-gray-700">
+            {page}
+          </button>
+          <button
+            onClick={() => setPage((page) => page + 1)}
+            disabled={books.length < 8}
+            className="py-1 px-2 rounded text-center border hover:bg-gray-700"
+          >
+            {page + 1}
+          </button>
         </div>
       </div>
-      {addEditModal && <AddEdit setModal={setAddEditModal} bookData={selectedBook} />}
+      {addEditModal && (
+        <AddEdit setModal={setAddEditModal} bookData={selectedBook} />
+      )}
     </main>
   );
 };
